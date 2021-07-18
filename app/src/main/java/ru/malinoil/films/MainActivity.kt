3e6e6864fd272a9873d000071223e96130766ebc
malinoile <evgeniyerr@gmail.com>
2021-07-18 12:48:54 +1000

    Добавлено нижнее и верхнее меню (без обработчиков)

--- app/src/main/java/ru/malinoil/films/MainActivity.kt
+++ app/src/main/java/ru/malinoil/films/MainActivity.kt
@@ -1,21 +1,26 @@
 package ru.malinoil.films
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import ru.malinoil.films.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private var listFilms = emptyList<Film>().toMutableList()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
         supportFragmentManager
             .beginTransaction()
             .add(binding.fragmentContainer.id, ListFragment())
             .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_navigation_menu, menu)
+        return true
     }
 }
